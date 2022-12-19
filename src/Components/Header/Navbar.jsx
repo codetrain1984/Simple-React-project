cdf33b8917802facedab0f56a156393f505b9569
gittrain <morezaaa84@gmail.com>
2022-12-19 22:57:54 +0330

    All parts got responsive

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,17 +1,22 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { BsTelephoneFill } from "react-icons/bs";
 import { FaPenNib } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+  };
   return (
     <div className="navbar">
       <div className="logoItems">
         <FaPenNib className="penLogo" />
         <h2>Course</h2>
       </div>
-      <ul className="navList">
+      <ul className={click ? "navList active" : "navList"}>
         <li className="navItems">
           <a className="navAnchor" href="#">
             Home
@@ -46,6 +51,9 @@ const Navbar = () => {
       <div className="navNumber">
         <BsTelephoneFill className="icon" />
         <p>+989123768818</p>
+      </div>
+      <div className="hamburger" onClick={handleClick}>
+        <GiHamburgerMenu className="hamLogo" />
       </div>
     </div>
   );
